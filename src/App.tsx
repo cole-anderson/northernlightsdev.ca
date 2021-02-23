5c1a0c99484e4c21a17ed6dcf63f86072d840c33
coleanderson <spedits@gmail.com>
2021-02-23 10:59:24 -0700

    start of  something new

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>Hello World</code> and save to reload.
+          <code>Hello World</code>
         </p>
         <a
           className="App-link"
