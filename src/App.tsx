1e8859e88e0d1ac2a206dd75bb47987f58ce4af2
coleanderson <spedits@gmail.com>
2021-02-23 12:47:26 -0700

    website live

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>Hello World</code>
+          <code>Blog Coming Soon!</code>
         </p>
         <a
           className="App-link"
