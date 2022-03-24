c31b4f15709357d7f1648031a62596dd36b61911
cole <spedits@gmail.com>
2022-03-23 20:40:52 -0600

    just doing some stuff

--- src/Components/Window.jsx
+++ src/Components/Window.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Draggable from 'react-draggable'
+import "./home.css"
 // Window widget that takes prop that determines what the window displays(options: terminal, blog, resume)
 
 export default function Window({ windowRoute }) {
     return (
         <Draggable>
-            <div className="box">
+            <div className="terminal-box">
                 <div>WindowRoute</div>
             </div>
         </Draggable>
